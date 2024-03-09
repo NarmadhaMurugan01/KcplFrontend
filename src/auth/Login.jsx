13ff2e1927b0e4695999b6f00b77f2a52a147a10
thirukumaran33008 <ThirukumaranV@kodukku.com>
2024-03-09 13:16:39 +0530

    Login password changes

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -90,9 +90,7 @@ const Login = () => {
       errors.password = "Password is required";
     } else if (values.password.length < 4) {
       errors.password = "Password must be more than 4 characters";
-    } else if (values.password.length > 10) {
-      errors.password = "Password cannot exceed more than 10 characters";
-    }
+    } 
     return errors;
   };
 
--- src/config/serverUrl.js
+++ src/config/serverUrl.js
@@ -10,3 +10,14 @@ module.exports = {
   authapi,
   ProfileImgApi
 };
+
+// const Nodeapi = 'http://localhost:8001/api';
+// const ResumeApi = 'http://localhost:8001/assets/UserResume/';
+// const ProfileImgApi = 'http://localhost:8001/assets/UserProfileImage/';
+// const authapi = 'http://localhost:8001';
+// module.exports = {
+//   Nodeapi,
+//   ResumeApi,
+//   authapi,
+//   ProfileImgApi
+// };
